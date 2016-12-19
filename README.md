6b3b6e2c749ca9235d59402a0ff16f070e89d890
Mateus Sales Bentes <mateusbentes@gmail.com>
2016-12-18 22:58:30 -0300

    new commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# README #
+
+First avaluation project!
+
+### What is this repository for? ###
+
+Academic study
+ 
+### How do I get set up? ###
+
+Netbeans necessary
+
+### Contribution guidelines ###
+
+Mateus Bentes, Sérgio Monteiro
+
+### Who do I talk to? ###
+
+* Repo owner or admin
+* Other community or team contact
---
+++ src/primeiravaliacao/terceiraimplementacao.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package primeiravaliacao;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author mateus
+ */
+public class terceiraimplementacao extends Primeiravaliacao {
+
+    double Resultado;
+    private final int limpanumero1 = 0;
+    private final int limpanumero2 = 0;
+    private final int limparesultado = 0;
+
+    void limpa() {
+        JOptionPane.showMessageDialog(null, "Resultado da multiplicacao limpo!!");
+    }
+
+    void entrarprograma() {
+        JOptionPane.showMessageDialog(null, "Voce esta multiplicando!!");
+    }
+
+    int getlimpa1() {
+        return limpanumero1;
+    }
+
+    void setlimpa1(int limpa) {
+        limpa = limpanumero1;
+    }
+
+    int getlimpa2() {
+        return limpanumero2;
+    }
+
+    void setlimpa2(int limpa) {
+        limpa = limpanumero2;
+    }
+
+    int getlimpa() {
+        return limparesultado;
+    }
+
+    void setlimpa(int limpa) {
+        limpa = limparesultado;
+    }
+
+    @Override
+    public double calcular(double x, double y) {
+        try {
+            return Resultado = x * y;
+        } catch (Exception e) {
+            return 0;
+        }
+    }
+}
